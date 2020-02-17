19f4403d01f9825d41da7135a92b9f33bfc11455
nutrii182 <nutrii182@gmail.com>
2020-02-16 19:24:28 -0600

    Formulario Cita Actualizado

--- coal/lib/src/pages/cita_page.dart
+++ coal/lib/src/pages/cita_page.dart
@@ -9,9 +9,13 @@ class CitaPage extends StatefulWidget {
 }
 
 class _CitaPageState extends State<CitaPage> {
+
+  String _motivo;
   String _fecha;
   DateTime _beginTime;
   DateTime _endTime;
+  DateTime _entrada = DateTime.parse('1970-01-01 09:00:00.000');
+	DateTime _salida = DateTime.parse('1970-01-01 18:00:00.000');
 
   @override
   Widget build(BuildContext context) {
@@ -23,25 +27,47 @@ class _CitaPageState extends State<CitaPage> {
                   child: Container(
                       padding: EdgeInsets.all(20.0),
                       child: ListView(
-                        children: <Widget>[
-                          _createDate(context),
-                          Divider(),
-                          _createBeginTime(context),
-                          Divider(),
-                          _createEndTime(context),
-                          SizedBox(height: 20.0),
-                          _createButtons(context)
-                        ],
+                          children: <Widget>[
+                            _createReason(context),
+                            Divider(),
+                            _createDate(context),
+                            Divider(),
+                            _createBeginTime(context),
+                            Divider(),
+                            _createEndTime(context),
+                            SizedBox(height: 20.0),
+                            _createButtons(context)
+                          ],
+                        ),
                       )),
-                )));
+                ));
+  }
+
+  Widget _createReason(BuildContext context){
+
+    return TextFormField(
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          labelText: 'Motivo',
+          hintText: 'Motivo de la Cita',
+          icon: Icon(Icons.web_asset)
+      ),
+      onChanged: (v) {
+        setState(() {
+          _motivo = v;
+        });
+      },
+    );
   }
 
   Widget _createDate(BuildContext context) {
+
     final format = DateFormat("dd-MM-yyyy");
 
     return DateTimeField(
       format: format,
-			
       decoration: InputDecoration(
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
           labelText: 'Fecha de la Cita',
@@ -107,9 +133,6 @@ class _CitaPageState extends State<CitaPage> {
 
   Widget _createButtons(BuildContext context) {
 
-		DateTime _entrada = DateTime.parse('1970-01-01 18:57:00.000');
-		DateTime _salida = DateTime.parse('1970-01-01 18:57:00.000');
-
     return Row(children: <Widget>[
       RaisedButton(
           color: Colors.blue,
@@ -118,16 +141,22 @@ class _CitaPageState extends State<CitaPage> {
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
           onPressed: () {
 						print(_beginTime);
-            if (_fecha == null || _beginTime == null || _endTime == null){
+            if (_fecha == null || _beginTime == null || _endTime == null || _motivo == null){
               Scaffold.of(context).showSnackBar(SnackBar(
                   backgroundColor: Colors.blue,
                   content: Text('No puede haber algún campo vacío')));
 						} else{
-							if(_endTime.isBefore(_beginTime) || _beginTime.isAfter(_endTime)
-							|| _beginTime.isBefore(_entrada) || _endTime.isAfter(_salida))
+							if(_endTime.isBefore(_beginTime) || _beginTime.isAfter(_endTime) ||
+              _beginTime.isBefore(_entrada) || _endTime.isAfter(_salida) || _beginTime == DateTime.now()
+              || _beginTime.isBefore(DateTime.now()))
 								Scaffold.of(context).showSnackBar(SnackBar(
 										backgroundColor: Colors.blue,
 										content: Text('Los Horarios son Inválidos')));
+              else{
+                Scaffold.of(context).showSnackBar(SnackBar(
+										backgroundColor: Colors.blue,
+										content: Text('Datos Correctos')));
+              }
 						}
           }),
       SizedBox(width: 15.0),
